725cc81d1252bed30777ef003137b250c1577894
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-04 20:37:33 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,11 @@
 function fetchCountries(name) { 
-    console.log('hello');
-    const r = fetch('https://restcountries.com/v3.1/name/ukraine');
+    // console.log('hello');
+    // const r = fetch('https://restcountries.com/v3.1/name/ukraine');
         
-        console.log(r);
+    //     console.log(r);
+    fetch('https://restcountries.com/v3.1/name/ukraine').then(data => {
+        console.log(data);
+    });
 }
 
 export { fetchCountries };
