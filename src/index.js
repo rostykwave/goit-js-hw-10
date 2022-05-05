655fb76ef36b333403a161e54586c6fa33fbca47
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-05 16:31:41 +0300

    functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import './css/styles.css';
+
+import countryCard from './templates/country-card.hbs'
 import { fetchCountries } from "./js/fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
 
-console.log(fetchCountries('ukraine'));
-console.log(fetchCountries('germany'));
+///References
+const refs = {
+    countryInfo: document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+}
+
+////Main code
+
+// fetchCountries('ukraine');
+// fetchCountries('germany');
+fetchCountries('italy').then(renderCountryInfo).catch(error => console.log(error));
+
+
+
+
+////Functions
+function renderCountryInfo(country) {
+    const markup = countryCard(country[0]);
+     refs.countryInfo.innerHTML = markup;
+}
 
 
 
