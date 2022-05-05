30f44ce61891b4e1935caa970f19357f5a364208
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-05 13:57:23 +0300

    logging html handlebars

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ const DEBOUNCE_DELAY = 300;
 
 console.log(fetchCountries('ukraine'));
 console.log(fetchCountries('germany'));
+
+
+
+////Знайти як дістати значення з об'єкту languages, коли для кожної країни клюя різний///done
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,30 @@
+// import countryCard from "../templates/country-card.hbs"
+import countryCard from '../templates/country-card.hbs'
+
 function fetchCountries(name) { 
-    // console.log('hello');
-    // const r = fetch('https://restcountries.com/v3.1/name/ukraine');
-        
-    //     console.log(r);
-    fetch(`https://restcountries.com/v3.1/name/${name}`)
+
+    fetch(`https://restcountries.com/v2/name/${name}?fields=flags,name,capital,population,languages`)
         .then(response => {
         return response.json();
     })
         .then(country => {
-        console.log(country);
+            console.log(country);
+            let markup = '';
+            country.forEach(el => {
+                console.log(el.flags.svg);
+                console.log(el.name);
+                console.log(el.capital);
+                console.log(el.population);
+                console.log(el.languages);
+                // console.log(Object.values(el.languages));
+
+                markup = countryCard(el);
+            });
+            
+    
+            
+            // const markup = countryCard(country);
+            console.log(markup);
     })
         .catch(error => {
         console.log(error);
@@ -19,4 +35,5 @@ export { fetchCountries };
 
 
 // https://restcountries.com/v3.1/name/${name}
+    //filtered 
 // https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population
