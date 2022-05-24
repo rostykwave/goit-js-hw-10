cbb65d8bdc94cd03a137d4aa275adcf400b008fc
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-24 16:50:24 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,5 @@
 const BASE_URL = 'https://restcountries.com/v2';
 const FILTER = '?fields=flags,name,capital,population,languages';
-// const FILTER = '';
 
 function fetchCountries(name) { 
     return fetch(`${BASE_URL}/name/${name}${FILTER}`)
@@ -9,7 +8,4 @@ function fetchCountries(name) {
 
 
 
-
 export { fetchCountries };
-
-///api-service.js - інша назва для цього файлу
